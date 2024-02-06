16f9a37b80014e136e44e912bd4c322d07181df6
sharathkumarviart <sharathkumarvirat@gmail.com>
2024-02-06 22:20:52 +0530

    delete space

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import App from './App.jsx'
 import {BrowserRouter  } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-
 <BrowserRouter basename ='/FindCoder_clone'>
   <App />
 </BrowserRouter>);
